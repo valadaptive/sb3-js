579fa629f9d4a5ee46e405c837cb343654e8a22b
valadaptive <valadaptive@protonmail.com>
2024-05-29 03:00:42 -0400

    Fail build if typechecking fails

--- rollup.config.js
+++ rollup.config.js
@@ -11,7 +11,7 @@ export default {
     },
     plugins: [
         nodeResolve({browser: true}),
-        typescript(),
+        typescript({noEmitOnError: true}),
         copy({
             targets: [
                 { src: 'src/assets/**/*', dest: 'dist/assets' }
