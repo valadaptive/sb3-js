f683f44d81b51cc999e6077272d31ec0a4746db7
valadaptive <valadaptive@protonmail.com>
2025-07-17 07:10:43 -0400

    Fix whitespace in shaders

--- src/renderer/shaders.ts
+++ src/renderer/shaders.ts
@@ -195,22 +195,22 @@ in vec2 v_texCoord;
 out vec4 fragColor;
 
 void main() {
-  // Maaaaagic antialiased-line-with-round-caps shader.
+    // Maaaaagic antialiased-line-with-round-caps shader.
 
-	// "along-the-lineness". This increases parallel to the line.
-	// It goes from negative before the start point, to 0.5 through the start to the end, then ramps up again
-	// past the end point.
-	float d = ((v_texCoord.x - clamp(v_texCoord.x, 0.0, u_lineLength)) * 0.5) + 0.5;
+    // "along-the-lineness". This increases parallel to the line.
+    // It goes from negative before the start point, to 0.5 through the start to the end, then ramps up again
+    // past the end point.
+    float d = ((v_texCoord.x - clamp(v_texCoord.x, 0.0, u_lineLength)) * 0.5) + 0.5;
 
-	// Distance from (0.5, 0.5) to (d, the perpendicular coordinate). When we're in the middle of the line,
-	// d will be 0.5, so the distance will be 0 at points close to the line and will grow at points further from it.
-	// For the "caps", d will ramp down/up, giving us rounding.
-	// See https://www.youtube.com/watch?v=PMltMdi1Wzg for a rough outline of the technique used to round the lines.
-	float line = distance(vec2(0.5), vec2(d, v_texCoord.y)) * 2.0;
-	// Expand out the line by its thickness.
-	line -= ((u_penThickness - 1.0) * 0.5);
-	// Because "distance to the center of the line" decreases the closer we get to the line, but we want more opacity
-	// the closer we are to the line, invert it.
-	fragColor = u_penColor * clamp(1.0 - line, 0.0, 1.0);
+    // Distance from (0.5, 0.5) to (d, the perpendicular coordinate). When we're in the middle of the line,
+    // d will be 0.5, so the distance will be 0 at points close to the line and will grow at points further from it.
+    // For the "caps", d will ramp down/up, giving us rounding.
+    // See https://www.youtube.com/watch?v=PMltMdi1Wzg for a rough outline of the technique used to round the lines.
+    float line = distance(vec2(0.5), vec2(d, v_texCoord.y)) * 2.0;
+    // Expand out the line by its thickness.
+    line -= ((u_penThickness - 1.0) * 0.5);
+    // Because "distance to the center of the line" decreases the closer we get to the line, but we want more opacity
+    // the closer we are to the line, invert it.
+    fragColor = u_penColor * clamp(1.0 - line, 0.0, 1.0);
 }
 `;
